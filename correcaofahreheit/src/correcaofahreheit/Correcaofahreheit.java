e809f87ad70d7db89cd05ac8251d260a04781500
Fernanda <fernandaferreira0s20@gmail.com.br>
2021-03-03 11:20:43 -0300

    Aula 03-03-2021

---
+++ correcaofahreheit/src/correcaofahreheit/Correcaofahreheit.java
@@ -0,0 +1,18 @@
+package correcaofahreheit;
+
+public class Correcaofahreheit {
+	
+	public static void main(String[] args) {
+		
+		double celsius = 27;
+		double fahrenheint;
+		
+		fahrenheint = (9 * celsius + 160) / 5;
+		
+		System.out.println(celsius + " graus celsius equivale a " +fahrenheint + " graus fahrenheint");
+		System.out.println(celsius + " " + fahrenheint);
+				
+				
+	}
+
+}
---
+++ fahrenheit/src/fahrenheit/ConversorCelsiusParaFahrenheit.java
@@ -0,0 +1,18 @@
+package fahrenheit;
+
+public class ConversorCelsiusParaFahrenheit {
+	
+	public static void main(String[] args) {
+		
+		float C = 27;
+		float valor9 = 9;
+		float valor160 = 160;
+		float valor5 = 5;
+		float F = (valor9 * C + valor160) / valor5;
+		
+		System.out.print("Fahrenheit: ");
+		System.out.println(F);
+	
+	}
+
+}
